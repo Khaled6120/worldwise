836ac4f1fae9f21ad77de0c99c844525a6ff5a39
Khaled6120 <khalednaes3@gmail.com>
2023-09-24 17:58:19 +0300

    Add delete a city functionality

--- src/contexts/CitiesContexts.jsx
+++ src/contexts/CitiesContexts.jsx
@@ -51,7 +51,20 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch (error) {
-      alert("Failed to fetch cities");
+      alert("There was an error creating the city.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting the city.");
     } finally {
       setIsLoading(false);
     }
@@ -59,7 +72,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
